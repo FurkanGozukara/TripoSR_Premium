a60929d1863bb1565a55da4da2b48b20c8b98ed6
FurkanGozukara <furkangozukara@gmail.com>
2025-10-23 20:18:23 +0300

    v3

--- gradio_app.py
+++ gradio_app.py
@@ -17,7 +17,7 @@ import argparse
 
 
 if torch.cuda.is_available():
-    device = "cuda:0"
+    device = "cuda"
 else:
     device = "cpu"
 
@@ -81,8 +81,7 @@ def run_example(image_pil):
 with gr.Blocks(title="TripoSR") as interface:
     gr.Markdown(
         """
-    # TripoSR Demo
-    [TripoSR](https://github.com/VAST-AI-Research/TripoSR) is a state-of-the-art open-source model for **fast** feedforward 3D reconstruction from a single image, collaboratively developed by [Tripo AI](https://www.tripo3d.ai/) and [Stability AI](https://stability.ai/).
+    # TripoSR Premium v1 by SECourses : https://www.patreon.com/posts/141896910
     
     **Tips:**
     1. If you find the result is unsatisfied, please try to change the foreground ratio. It might improve the results.
@@ -117,24 +116,24 @@ with gr.Blocks(title="TripoSR") as interface:
                         label="Marching Cubes Resolution",
                         minimum=32,
                         maximum=320,
-                        value=256,
+                        value=320,
                         step=32
                     )
             with gr.Row():
                 submit = gr.Button("Generate", elem_id="generate", variant="primary")
         with gr.Column():
+            with gr.Tab("GLB"):
+                output_model_glb = gr.Model3D(
+                    label="Output Model (GLB Format)",
+                    interactive=False,
+                )
+                gr.Markdown("Note: The model shown here has a darker appearance. Download to get correct results.")            
             with gr.Tab("OBJ"):
                 output_model_obj = gr.Model3D(
                     label="Output Model (OBJ Format)",
                     interactive=False,
                 )
                 gr.Markdown("Note: The model shown here is flipped. Download to get correct results.")
-            with gr.Tab("GLB"):
-                output_model_glb = gr.Model3D(
-                    label="Output Model (GLB Format)",
-                    interactive=False,
-                )
-                gr.Markdown("Note: The model shown here has a darker appearance. Download to get correct results.")
     with gr.Row(variant="panel"):
         gr.Examples(
             examples=[
@@ -183,7 +182,5 @@ if __name__ == '__main__':
     interface.queue(max_size=args.queuesize)
     interface.launch(
         auth=(args.username, args.password) if (args.username and args.password) else None,
-        share=args.share,
-        server_name="0.0.0.0" if args.listen else None, 
-        server_port=args.port
+        share=args.share,inbrowser=True
     )
